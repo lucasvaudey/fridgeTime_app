712dc5e77d0a88a2683084a8ae90de4f9811d073
lucasvaudey <lucasvaudey@gmail.com>
2021-05-07 15:41:27 +0200

    deconnect class done.

--- app/src/main/java/com/example/fridgetime/activities/RegisterLogin.java
+++ app/src/main/java/com/example/fridgetime/activities/RegisterLogin.java
@@ -9,7 +9,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.fridgetime.R;
@@ -24,55 +26,104 @@ public class RegisterLogin extends AppCompatActivity {
 
     private RegisterLoginPOST registerLoginPOST;
     private EditText email;
+    private TextView email_title;
     private EditText username;
+    private TextView username_title;
     private EditText password;
+    private Button buttonChange;
+    private Button lorR;
     private SharedPreferences sharedPreferences;
+    private static Boolean login = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_login);
         email = findViewById(R.id.email_edit);
+        email_title = findViewById(R.id.email_title);
+        username_title = findViewById(R.id.username_title);
         username = findViewById(R.id.username_edit);
         password = findViewById(R.id.password_edit);
+        buttonChange = findViewById(R.id.alreadyAnAccount);
+        lorR = findViewById(R.id.registerLogin);
         registerLoginPOST = new RegisterLoginPOST();
         sharedPreferences = getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE);
     }
 
-    public void register(View view) throws ExecutionException, InterruptedException, JSONException {
+    public void registerLogin(View view) throws ExecutionException, InterruptedException, JSONException {
         String usernameText = username.getText().toString();
         String emailText = email.getText().toString();
         String passwordText = password.getText().toString();
-        JSONObject jsonResponse;
-        jsonResponse = registerLoginPOST.register(usernameText, emailText, passwordText).get();
-        switch (jsonResponse.getInt("success")) {
-            case 0:
-                switch (jsonResponse.getString("field")) {
-                    case "password":
-                        password.setError(jsonResponse.getString("message"));
-                        break;
-                    case "email":
-                        email.setError(jsonResponse.getString("message"));
-                        break;
-                    case "username":
-                        username.setError(jsonResponse.getString("message"));
-                        break;
-                    case "none":
-                        Toast.makeText(this, "Une erreur interne s'est produit veuillez réessayez" +
-                                " plus tard", Toast.LENGTH_LONG).show();
-                        break;
-                }
-                break;
-            case 1:
-                Intent intent = new Intent(this, HomePage.class);
-                sharedPreferences = getApplicationContext().getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE);
-                SharedPreferences.Editor editor = sharedPreferences.edit();
-                editor.putString("sessionID", jsonResponse.getString("sessionID"));
-                editor.apply();
-                Log.d("tag", "on a bien mis" + jsonResponse.getString("sessionID") + sharedPreferences.getString("sessionID", null));
-                startActivity(intent);
-                break;
+        if(emailText.equals("") && !login){
+            email.setError("Le champs est vide.");
+        }else
+        if(usernameText.equals("")){
+            username.setError("Le champs est vide.");
+        }else
+        if(passwordText.equals("")){
+            password.setError("Le champs est vide.");
+        }else {
+            JSONObject jsonResponse;
+            if(!login){
+                jsonResponse = registerLoginPOST.register(usernameText, emailText, passwordText).get();
+            }else{
+                jsonResponse = registerLoginPOST.login(usernameText, passwordText).get();
+            }
+            switch (jsonResponse.getInt("success")) {
+                case 0:
+                    switch (jsonResponse.getString("field")) {
+                        case "password":
+                            password.setError(jsonResponse.getString("message"));
+                            break;
+                        case "email":
+                            email.setError(jsonResponse.getString("message"));
+                            break;
+                        case "usernameOrEmail":
+                            username.setError(jsonResponse.getString("message"));
+                            break;
+                        case "username":
+                            username.setError(jsonResponse.getString("message"));
+                            break;
+                        case "none":
+                            Toast.makeText(this, "Une erreur interne s'est produit veuillez réessayez" +
+                                    " plus tard", Toast.LENGTH_LONG).show();
+                            break;
+                    }
+                    break;
+                case 1:
+                    Intent intent = new Intent(this, HomePage.class);
+                    sharedPreferences = getApplicationContext().getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putString("sessionID", jsonResponse.getString("sessionID"));
+                    editor.apply();
+                    startActivity(intent);
+                    break;
+            }
         }
+    }
 
+    public void loginLayout(View view) {
+        login = !login;
+        if(login){
+            email.setVisibility(View.GONE);
+            email.setText("");
+            password.setText("");
+            username.setText("");
+            email_title.setVisibility(View.GONE);
+            username_title.setText(R.string.usernameOrEmail);
+            username.setHint(R.string.usernameOrEmail);
+            buttonChange.setText(R.string.creer_un_compte);
+            lorR.setText(R.string.se_connecter);
+        }else{
+            email.setVisibility(View.VISIBLE);
+            email_title.setVisibility(View.VISIBLE);
+            email.setText("");
+            password.setText("");
+            username.setText("");
+            username_title.setText(R.string.username_title);
+            username.setHint(R.string.username_title);
+            buttonChange.setText(R.string.changeButton_text);
+            lorR.setText(R.string.s_enregistrer);
+        }
     }
 }
